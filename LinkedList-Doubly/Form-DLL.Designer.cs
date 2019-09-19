5021ec33d97131d11d241720d1ddb2a95d95ac51
NguyenMinhSang <nguyenminhsang5692@gmail.com>
2019-09-19 12:49:03 +0700

    Initial commit

---
+++ LinkedList-Doubly/Form-DLL.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace LinkedList_Doubly
+{
+    partial class LinkedListDB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(306, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 144);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(682, 286);
+            this.panel1.TabIndex = 3;
+            // 
+            // LinkedListDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 430);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "LinkedListDB";
+            this.Text = "LinkedListDB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ LinkedList-Doubly/Form-DLL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkedList_Doubly
+{
+    public partial class LinkedListDB : Form
+    {
+        public LinkedListDB()
+        {
+            InitializeComponent();
+        }
+
+        LinkedListDoubly.LinkedList_Doubly LLDB = new LinkedListDoubly.LinkedList_Doubly();
+
+        Button[] bt;
+        Point startpoint, endpoint;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point pt = Cursor.Position;
+            startpoint = pt;
+            endpoint = pt   ;
+            string[] arr = textBox1.Text.Split(';');
+            Array.Sort(arr);
+            bt = new Button[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bt[i] = new Button();
+                this.Controls.Add(bt[i]);
+                bt[i].Location = new Point(50 + i * 50, 200);
+                bt[i].Name = "button" + i;
+                bt[i].Size = new Size(50, 30);
+                bt[i].TabIndex = i;
+                bt[i].Text = arr[i];
+                bt[i].UseVisualStyleBackColor = true;
+                //bt[i].Click += new System.EventHandler(this.button1_Click);
+                System.Drawing.Pen myPen;
+                myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+                System.Drawing.Graphics formGraphics = this.CreateGraphics();
+                formGraphics.DrawLine(myPen, 0, 0, 200, 200);
+                myPen.Dispose();
+                formGraphics.Dispose();
+            }
+        }
+    }
+}
+
